0729c91017f95fa97ecbc1fcff1f93518ebce33d
신지윤 <ranji12@naver.com>
2024-09-22 23:28:32 +0900

    python test

---
+++ inout.py
@@ -0,0 +1,9 @@
+# my_name = "공욱재"
+my_name = input("내 이름을 입력하세요:")
+
+def print_name(sung, name):
+    print(f"저의 성씨는 {sung}이고 이름은 {name} 입니다.")
+
+array = []
+array.push(my_name)
+print_name(array[0],array[1])
---
+++ testOne.py
@@ -0,0 +1,27 @@
+a = "공욱재"
+b = "미남"
+
+def string_merge():
+  global a, b
+  return a + b
+
+print(string_merge())
+
+
+# 맨날 이름 쓰기 힘듦
+def example():
+  return "예제"
+
+# 익명함수 (세련)
+another_example = lambda: "예제"
+
+
+array = ["배성빈", "문혜림", "신지윤"]
+
+obj = {
+  "조자연": "안녕",
+  "김보미": "개굴",
+  "활재민": "수면왕"
+}
+
+another_obj = ("김정수", "송이현", "최유잔")
